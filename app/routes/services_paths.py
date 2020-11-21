c19d268bc257c343ec906b89e2ec0160e6a97454
Petr Alekseev <p.g.alekseev@tinkoff.ru>
2020-11-21 13:24:13 +0300

    Additional probes

--- app/routes/services_paths.py
+++ app/routes/services_paths.py
@@ -6,9 +6,14 @@ async def probe_handler(request: Request) -> Response:
     return JSONResponse(status_code=204)
 
 
+async def z_handler(request: Request) -> Response:
+    return JSONResponse(status_code=200)
+
 paths = {
     'probes': [
         ('/livenessProbe/', 'GET', probe_handler, 'liveness', 204),
         ('/readinessProbe/', 'GET', probe_handler, 'readiness', 204),
+        ('/healthz/', 'GET', z_handler, 'healthz', 200),
+        ('/readyz/', 'GET', z_handler, 'readyz', 200),
     ]
 }
