c22ad905454ce02360fd4baff884fc0bc48e9d7c
Petr Alekseev <p.g.alekseev@tinkoff.ru>
2020-11-21 13:29:59 +0300

    Create db tables before start server

--- cli.py
+++ cli.py
@@ -9,6 +9,11 @@ from app.database import db
 def cli() -> None:
     pass
 
+def create_db():
+    db.connect()
+    db.create_tables([db_models.User, db_models.Cost, db_models.Action, db_models.Photo, db_models.Message])
+    db.close()
+    click.secho("\nCreate DB tables!", fg='green')
 
 @cli.command()
 def generate() -> None:
@@ -35,6 +40,8 @@ def runserver() -> None:
     """
     from app.settings import settings
     from main import app
+
+    create_db()
 
     uvicorn.run(
         app,
